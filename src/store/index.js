a20323491d269875a97e70859f50dd72521dd6cf
Ravi Singh Bajetha <84458346+ravi-singh-100@users.noreply.github.com>
2023-07-18 23:26:57 +0530

    Add files via upload

---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+// import {createStore} from 'redux'
+import {configureStore} from '@reduxjs/toolkit'
+const counterReducer=(state={counter:0},action)=>{
+if(action.type==='increment'){
+    return {
+        counter:state.counter+1
+    }
+}
+if(action.type==='decrement')
+{
+    return {
+        counter:state.counter-1
+    }
+}
+return state;
+}
+const store=configureStore({  reducer: {
+    counterReducer:counterReducer
+  },});
+export default store;
